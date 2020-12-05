ae850e406b1c7f269d9f254411fd086d50ba8ee7
Cory Murphy <public@coryamurphy.com>
2020-12-05 18:25:30 -0500

    finish part 2

--- day3/main.go
+++ day3/main.go
@@ -25,21 +25,67 @@ func part1() {
 
 func startToboggan(tobogganHill hill) hill {
 
+	// I'm not proud of this, but I have my reasons :)
+
 	horizontal := 0
+	horizontal1 := 0
+	horizontal3 := 0
+	horizontal5 := 0
+	horizontal7 := 0
+
 	vertical := 0
-	trees := 0
+	vertical2 := 0
+	trees1 := 0
+	trees3 := 0
+	trees5 := 0
+	trees7 := 0
+	treesVert2 := 0
+
 	for vertical < len(tobogganHill)-1 {
 
 		vertical++
-		horizontal = horizontal + 3
+		horizontal1++
+		horizontal3 = horizontal3 + 3
+		horizontal5 = horizontal5 + 5
+		horizontal7 = horizontal7 + 7
 
-		if tobogganHill[vertical][horizontal] {
-			trees++
+		if tobogganHill[vertical][horizontal1] {
+			trees1++
+		}
+
+		if tobogganHill[vertical][horizontal3] {
+			trees3++
+		}
+
+		if tobogganHill[vertical][horizontal5] {
+			trees5++
+		}
+
+		if tobogganHill[vertical][horizontal7] {
+			trees7++
 		}
 
 	}
 
-	fmt.Printf("trees encountered: %s\n", fmt.Sprint(trees))
+	for vertical2 < len(tobogganHill)-1 {
+
+		vertical2 = vertical2 + 2
+		horizontal++
+
+		if tobogganHill[vertical2][horizontal] {
+			treesVert2++
+		}
+
+	}
+
+	fmt.Printf("trees encountered horizontal1: %s\n", fmt.Sprint(trees1))
+	fmt.Printf("trees encountered horizontal3: %s\n", fmt.Sprint(trees3))
+	fmt.Printf("trees encountered horizontal5: %s\n", fmt.Sprint(trees5))
+	fmt.Printf("trees encountered horizontal7: %s\n", fmt.Sprint(trees7))
+	fmt.Printf("trees encountered vert2: %s\n", fmt.Sprint(treesVert2))
+
+	final := trees1 * trees3 * trees5 * trees7 * treesVert2
+	fmt.Printf("all trees multiplied: %s\n", fmt.Sprint(final))
 	return tobogganHill
 }
 
@@ -92,7 +138,7 @@ func initHill(lines []string) hill {
 	for _, line := range lines {
 		i := 0
 		horiz := horizontal{}
-		for i < 32 {
+		for i < 128 {
 			for _, plot := range strings.Split(line, "") {
 				horiz = append(horiz, parseTree(plot))
 			}
