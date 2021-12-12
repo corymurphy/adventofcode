3141407ed2e9dc328288c97faa7341b2e7bb5f76
Cory Murphy <cory.murphy@ezcater.com>
2021-12-12 17:10:30 -0500

    start day 5

---
+++ 2021/day5/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := readInput("input")
+
+	part1 := part1(input)
+	part2 := part2(input)
+
+	fmt.Printf("\nPart 1 answer: %d\n\n", part1)
+	fmt.Printf("\nPart 2 answer: %d\n\n", part2)
+}
+
+func part1(input []string) int {
+	return 0
+}
+
+func part2(input []string) int {
+	return 0
+}
