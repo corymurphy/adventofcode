700958aae89d8543f23036e248c8b31a3e8526dc
corymurphy <public@corymurphy.io>
2025-12-02 06:52:44 -0500

    create a template day

--- 2025/day1/main_test.go
+++ 2025/day1/main_test.go
@@ -7,28 +7,28 @@ import (
 )
 
 func Test_Part1_Sample(t *testing.T) {
-	expected := 0
+	expected := 3
 	input := shared.ReadInput("input_sample")
 	actual := part1(input)
 	shared.AssertEqual(t, expected, actual)
 }
 
 func Test_Part1(t *testing.T) {
-	expected := 0
+	expected := 1191
 	input := shared.ReadInput("input")
 	actual := part1(input)
 	shared.AssertEqual(t, expected, actual)
 }
 
 func Test_Part2_Sample(t *testing.T) {
-	expected := 0
+	expected := 6
 	input := shared.ReadInput("input_sample")
 	actual := part2(input)
 	shared.AssertEqual(t, expected, actual)
 }
 
 func Test_Part2(t *testing.T) {
-	expected := 0
+	expected := 6858
 	input := shared.ReadInput("input")
 	actual := part2(input)
 	shared.AssertEqual(t, expected, actual)
