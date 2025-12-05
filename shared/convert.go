ab4b59fea1702f4d32f405d5809a81cfe82efa35
corymurphy <public@corymurphy.io>
2025-12-05 05:18:26 -0500

    add int64 helper

--- shared/convert.go
+++ shared/convert.go
@@ -6,3 +6,11 @@ func ToInt(input string) int {
 	value, _ := strconv.ParseInt(input, 10, 32)
 	return int(value)
 }
+
+func MustInt64(input string) int64 {
+	val, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		panic("unable to convert")
+	}
+	return val
+}
